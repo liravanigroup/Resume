7ab77ac93ceade9d4266fb69cf50c7c8ea1a9756
Sergej Povzaniuk <liravanigroup@gmail.com>
2016-04-30 10:48:52 +0200

    Add LINK

---
+++ src/pl/com/bottega/webapp/model/Link.java
@@ -0,0 +1,26 @@
+package pl.com.bottega.webapp.model;
+
+/**
+ * Sergej Povzaniuk
+ * 2016-04-30.
+ */
+public class Link {
+    private String name;
+    private String url;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
