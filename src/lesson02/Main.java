d5b8a30a3f03ba1b59b69055bcf0626175be4bbe
Sergej Povzaniuk <liravanigroup@gmail.com>
2016-04-30 10:52:53 +0200

    Rename package and add package lesson02

---
+++ src/lesson02/Main.java
@@ -0,0 +1,11 @@
+package lesson02;
+
+/**
+ * Resume
+ * Sergii
+ * 2016-04-30.
+ */
+
+public class Main {
+
+}
--- src/pl/com/bottega/webapp/model/Link.java
+++ src/pl/com/bottega/webapp/model/Link.java
@@ -5,22 +5,19 @@ package pl.com.bottega.webapp.model;
  * 2016-04-30.
  */
 public class Link {
-    private String name;
-    private String url;
+    private final String name;
+    private final String url;
+
+    public Link(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
     }
 }
